c51952a98f1c13031846ac3c0d0715cca645b974
Dmitry <muaydimon@mail.ru>
2022-08-02 20:27:30 +0300

    Отправляет сообщение в телеграм с графиком курсов валют за месяц пользователям добавленным в db. Добавляет id пользователей написавших боту (в течении суток) в db. Курс валют USD и EURO берется из API нбрб

---
+++ my_bot.py
@@ -0,0 +1,115 @@
+import telebot
+from bot_config import token
+import nbrb
+import requests
+from mysql_config import *
+
+
+bot = telebot.TeleBot(token)
+
+
+def bot_id_and_name_user():
+    """
+    функция возвращает словарь с ключами 'chat_id' и 'name' юзера, который последний написал боту в л/с
+    """
+    response = requests.get(f'https://api.telegram.org/bot{token}/getupdates')
+    dict_jsn = response.json()
+    # преобразовали response в класс dict
+    spis = dict_jsn['result']
+    # вытянули вложеный список из словаря с ключем 'result' (1 итерация списка это 1 сообщение пользователя)
+    #pprint(spis)
+    for i in spis:
+        if 'message' in i:
+            # если есть ключ 'message' в списке сообщений от пользователя выводим ID и Имя пользователя
+            chat_id = int(i['message']['from']['id'])  # int
+            chat_name = i['message']['from']['first_name']  # str
+        else:
+            # если есть нет ключа 'message', пропускаем итерацию
+            continue
+    return dict(chat_id=chat_id, name=chat_name)
+
+
+def db_add_users(result):
+    """
+    функция добавляет в БД юзеров которые написали боту в л/с, если такие отсутствуют в БД
+    """
+    try:
+        connection = pymysql.connect(host=host,
+                                     user=user,
+                                     port=3306,
+                                     password=password,
+                                     database=database,
+                                     charset=charset,
+                                     cursorclass=cursorclass)
+        print("connect... OK")
+
+        try:
+            with connection.cursor() as cursor:
+                cursor.execute('select * from users;')
+                rows = cursor.fetchall()
+                print(rows)  # вывод в консоль список добавленых юзеров в базу данных
+                for row in rows:
+                    # если юзер, который написал боту существует в БД, тогда пропускаем. иначе добавляем его в БД
+                    if row == result:
+                        break
+                    else:
+                        cursor.execute('use test;')
+                       # selec = cursor.execute('select * from users;')
+                        cursor.execute(
+                            f'insert into users (chat_id, name) values ({result["chat_id"]},'
+                                       f' \'{result["name"]}\');'
+                                       )  # добавляем запись в таблицу
+                        connection.commit()  # метод для сохранения изменений
+                        print("новый user успешно добавлен в  таблицу")
+
+        finally:
+            connection.close()
+            print("connection close... OK")
+
+    # Если ошибка в подключении, то выводим тип ошибки
+    except Exception as ex:
+        print("no connect...")
+        print(ex)
+
+
+def users_list_db():
+    """
+    возвращает список id добавленных в БД
+    """
+    chat_id = []
+    try:
+        connection = pymysql.connect(host=host,
+                                     user=user,
+                                     port=3306,
+                                     password=password,
+                                     database=database,
+                                     charset=charset,
+                                     cursorclass=cursorclass)
+        try:
+            with connection.cursor() as cursor:
+                cursor.execute('select * from users;')
+                rows = cursor.fetchall()
+                for row_id in rows:
+                    chat_id.append(row_id['chat_id'])
+                # print(rows)  # вывод в консоль список добавленых юзеров в базу данных
+        finally:
+            connection.close()
+
+    except Exception as ex:
+        print("no connect...")
+        print(ex)
+    return chat_id
+
+
+def answer(users_id):
+    """
+    функция отправляет сообщение от бота с графиком курса валют в л/с юзера
+    """
+    for user in users_id:
+        nbrb.curse_per_week()
+        photo = open('C:\\Users\\USER\\PycharmProjects\\pythonProject1\\График.png', 'rb')
+        bot.send_photo(user, photo)
+
+
+if __name__ == '__main__':
+    answer(users_list_db())
---
+++ nbrb.py
@@ -0,0 +1,60 @@
+import requests
+from json import loads
+import matplotlib.pyplot as plt
+from datetime import timedelta
+from datetime import date
+
+
+def curse_per_week():
+    x = []
+    y = []
+    y_usd = []
+    data_days = -31
+
+    for i in range(data_days, 2):
+        data = str(date.today() + timedelta(i))  # дата за неделю + 1 день
+        url = 'https://www.nbrb.by/api/exrates/rates/Cur_Abbreviation=EUR?ondate=' + data + '&Cur_ID=451'
+        url_usd = 'https://www.nbrb.by/api/exrates/rates/Cur_Abbreviation=USD?ondate=' + data + '&Cur_ID=431'
+        response_usd = requests.get(url_usd)
+        response = requests.get(url)  # print(response) 200, если все ок
+
+        if response.ok and response_usd.ok:
+            response_text_usd = response_usd.text
+            response_text = response.text  # текст дата, валюта, курс и т.д. type str
+            dic_response_text_usd = loads(response_text_usd)
+            dic_response_text = loads(response_text)  # response_text type str в type dict
+            curs_response_usd = dic_response_text_usd['Cur_OfficialRate']
+            curs_response = dic_response_text['Cur_OfficialRate']  # вывод курса евро в переменную type(float)
+            y.append(curs_response)  # переводим значение курса в type(list)
+            y_usd.append(curs_response_usd)
+            slice_data = dic_response_text['Date']
+            slice_data = slice_data[0:10]
+            x.append(slice_data)  # вывод даты в переменную type(list)
+        else:
+            break
+
+    eur = plt.subplot(2, 1, 1)
+    plt.plot(x, y, '-ob')
+    plt.legend(['Курс EUR'])
+    eur.tick_params(pad=1,  # Расстояние между черточкой и ее подписью
+                    labelsize=6,  # Размер подписи
+                    labelcolor='b',  # Цвет подписи
+                    labelrotation=60)  # Поворот подписей
+    plt.subplots_adjust(hspace=0.4)
+
+    usd = plt.subplot(2, 1, 2)
+    plt.plot(x, y_usd, '-og')
+    plt.legend(['Курс USD'])
+    usd.tick_params(pad=1,  # Расстояние между черточкой и ее подписью
+                    labelsize=6,  # Размер подписи
+                    labelcolor='g',  # Цвет подписи
+                    labelrotation=60)  # Поворот подписей
+
+    eur.grid()
+    usd.grid()
+    plt.suptitle('Курсы валют EUR и USD по отношению к BYN')
+    plt.savefig('График.png')
+    # plt.show()
+
+
+# curse_per_week()
